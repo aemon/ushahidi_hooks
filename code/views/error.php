83ebb0c46003f13b5c923d17009011d47de2e6bf
aemon <aemon@maps.112.ua>
2013-11-27 10:07:24 +0200

    added 404 page and stats fixed

---
+++ code/views/error.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title><?php echo $title ?></title>
+<?php echo html::stylesheet('media/css/error_112'); ?>
+</head>
+
+<body>
+
+
+<div id="container">
+	<div id="container-big">
+    <div id="container-up">
+      <div id="container-text">
+        <div class="h1"><img src="/media/img/112/404.png" id="404" alt="404"></div>
+
+					<div class="h2">Запрашиваемая Вами страница не найдена!</div>
+            <div id="central_con">
+            <spawn class="container-bootom-text">
+             <spawn class="text"> Что случилось?</spawn>
+             <p>
+             Вы перешли по неработающей ссылке.<br>
+             Вы набрали адрес с ошибкой.<br>
+             Возможно, эта страница удалена.
+             </p>
+             </spawn>
+             <spawn class="container-bootom-text1">
+             <spawn class="text"> Что делать?</spawn>
+             <p>
+             Проверьте правильность адреса страницы.<br>
+             Вернитесь на главную страницу или попробуйте найти нужный<br>
+             материал в меню ниже.
+             </p>
+             </spawn>
+
+              <spawn class="url"><a href="/">ПЕРЕЙТИ НА ГЛАВНУЮ СТРАНИЦУ</a></spawn>
+               </div>
+               </div>
+                <div class="cont">
+
+        <spawn class="log" itemprop="log"><a itemprop="url" id="banner_link" href="/"></a></spawn>
+        <spawn class="disp"></spawn>
+        <spawn class="info">
+       Новое информационное агентство 112.ua<br>
+              - это самые свежие новости и<br>
+              события Украины и мира
+      </spawn>
+       </div>
+     </div>
+  </div>
+
+
+				<!-- End of the upper half of container !-->
+						<div id="bottom_menu">
+						<div class="header">
+						<ul class="menu">
+
+					<li class="item first"><a href="http://112.ua">Главная</a></li>
+					<li class="item"><a href="http://112.ua/glavnye-novosti">Новости</a></li>
+					<li class="item"><a href="http://map.112.ua/sobytiya">События</a></li>
+					<li class="item"><a href="http://map.112.ua/ochevidec">Я - очевидец!</a></li>
+					<li class="item"><a href="http://112.ua/video">Видео</a></li>
+          <li class="item"><a href="http://112.ua/photo">Фото</a></li>
+					<li class="item"><a href="http://map.112.ua/karta">Карта</a></li>
+					<li class="item last"><a href="http://map.112.ua/message">Обратная связь</a></li>
+
+				</ul>
+				</div>
+				</div>
+
+		<!-- Start of footer !-->
+
+		<div id="footer">
+
+		</div>
+
+		<!-- End of footer !-->
+
+	</div>
+
+
+</body>
+</html>
